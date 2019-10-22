446435dc70c399658c07cdacfe2f0394c9ee68a0
example <email@example.com>
2019-10-22 16:24:48 +0300

    msg

---
+++ GitHub2.java
@@ -0,0 +1,15 @@
+package GitHub;
+
+public class GitHub2 {
+
+	public static void main(String[] args) {
+		  int sayi1 = 10;
+	        int sayi2 = 20;
+	 
+	        int toplam = sayi1 + sayi2;
+	 
+	        System.out.println("Sayýlarýn Toplamý: " + toplam);
+
+	}
+
+}
